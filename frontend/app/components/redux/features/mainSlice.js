df30eb0ef72cac6777e219267946728da9fe7f17
kiyotone <kunwarkirtan36@gmail.com>
2023-10-08 02:42:28 +0545

    Search Bar updated

--- frontend/app/components/redux/features/mainSlice.js
+++ frontend/app/components/redux/features/mainSlice.js
@@ -3,6 +3,8 @@ import { createSlice , PayloadAction } from "@reduxjs/toolkit";
 const initialState = {
     search: "",
     current_display : [],
+    initial_date : "2005-02-12",
+    final_date : "2020-02-12",
 
 }
 
@@ -18,9 +20,15 @@ export const main = createSlice({
         setDisplay: (state, action) => {
             state.current_display = action.payload;
         },
+        setInitialDate: (state, action) => {
+            state.initial_date = action.payload;
     },  
+    setFinalDate: (state, action) => {
+        state.final_date = action.payload;
+    }
+}
 
 })
 
-export const { setSearch ,setDisplay } = main.actions;
+export const { setSearch ,setDisplay ,setInitialDate,setFinalDate } = main.actions;
 export default main.reducer; 
