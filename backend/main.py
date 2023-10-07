0a529a8509768b9ad0d253d406d62277d714c2ca
Prateek61 <prateekpoudel61@gmail.com>
2023-10-07 16:36:49 +0545

    implemented basic search functionality

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from search_func import search_query
 
 app = FastAPI()
 
@@ -15,3 +16,7 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/search")
+async def search(query: str ,skip: int = 0, limit: int = 10):
+    return search_query(query=query, skip=skip, limit=limit)
