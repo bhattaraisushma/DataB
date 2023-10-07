23f2d300eb63c67938f5ac10bdc89f8983a30104
kiyotone <kunwarkirtan36@gmail.com>
2023-10-07 13:42:28 +0545

    store updating

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -1,6 +1,11 @@
+"use client"
 import React from 'react'
+import { useState } from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
 const Navbar = () => {
+
+  const [search,change_seach] = useState();
+
   return (
     <>
       <div className="navbar bg-base-100">
@@ -11,9 +16,10 @@ const Navbar = () => {
     <a className="btn btn-ghost normal-case text-xl">Sun</a>
     <a className="btn btn-ghost normal-case text-xl">Moon</a>
     </div>
-    <input type ="text" className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem]  w-[50rem] rounded-[12rem]"/>
+    <input type ="text" className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem] w-[50rem] rounded-[12rem]" onChange={(e)=> change_seach(e.target.value)}/>
     <p className='text-[2rem] absolute ml-[81rem]'> <AiOutlineSearch/></p>
-  
+        {search}
+
   </div>
 </div>
 
