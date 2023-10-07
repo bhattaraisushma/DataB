2e9d30ef5a51ee87ff83aabbc77c9682e38a3e55
bhattaraisushma <bhattaraisushma260@gmail.com>
2023-10-07 16:10:06 +0545

    componet

--- frontend/app/components/Leftside.js
+++ frontend/app/components/Leftside.js
@@ -14,8 +14,8 @@ function Leftside() {
         <div>
       
         <div className=" bg-white w-screen    mt-[22rem]  pl-[10rem] rounded-2xl flex flex-row">
-        <Image  className="h-[20rem]  w-[20rem] " src={photo}/>
-   <div><p className='text-red-600  text-[2rem] font-semibold mt-14'>About data.gov</p>
+      <div >  <Image  className="h-[20rem]  w-[20rem]  mt-[1rem]" src={photo}/></div>
+   <div><p className='text-red-600  text-[2rem] font-semibold mt-[5rem]'>About data.gov</p>
     <p className='text-black text-lg w-[40rem]'>Lorem ipsum dolor sit amet consectetur dipisicing elit. Officiis ea provident beatae maiores neque cum adipisci a eaque. Fugit, accusantium! Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem deserunt culpa odit debitis pariatur suscipit eligendi doloremque deleniti ducimus.</p>
     <button className="btn btn-info rounded-[12rem]  bg-green-400 w-[8rem] text-white mt-[5rem]  ml-10">Info</button>
   </div>
