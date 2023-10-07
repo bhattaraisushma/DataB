dc0fc852bb257e6d3a26d63c7de0f4efd3ad4a9c
kiyotone <kunwarkirtan36@gmail.com>
2023-10-08 05:44:29 +0545

    rounded added

--- frontend/app/components/Data.js
+++ frontend/app/components/Data.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
+import imamge from '../../public/default.png'
+import Image from 'next/image';
 
 
 function Data({data}) {
@@ -11,7 +13,13 @@ function Data({data}) {
      <div className=' rounded-md bg-[#3b3b3b] h-full text-cyan-100 '>
         <div className='flex items-center space-x-6'>
 
-        <img className=' text-white w-[4rem] h-[3rem]' src={data.photo}/>
+        {
+          !data.photo || data.photo === "" ?
+          <Image className=' text-white w-[4rem] rounded-full h-[3rem]' src={imamge}/>
+          :
+          
+        <img className=' text-white w-[4rem] rounded-full h-[3rem]' src={data.photo}/>
+        }
         <span className='text-lg'>{data.title}</span>
         
         </div>
