105f9ec1b91415e6b55fa6c7c006b91ca50e125d
bhattaraisushma <bhattaraisushma260@gmail.com>
2023-10-07 16:10:06 +0545

    componet

--- frontend/app/components/Rightside.js
+++ frontend/app/components/Rightside.js
@@ -5,19 +5,16 @@ import photo from '../../public/Geoimg-removebg-preview.png'
 function Rightside() {
   return (
     <div >
-      <div className='pl-[40rem]'>
+      <div className='pl-[40rem] '>
       <Image className=" mt-7 w-[40rem] "src={photo}/>
       </div>
       <div>
-        <section>
-          <div className="join join-vertical">
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Most Viewed Datasets</button>
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Recently Added Datasets</button>
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Datasets by Organization</button>
-          </div>
+     
+          
       
-        </section>
+        
       </div>
+   
     </div>
   )
 }
