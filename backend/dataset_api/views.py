fa5a60641674978ad023a9bb5bb6624ea9986824
Prateek61 <prateekpoudel61@gmail.com>
2023-10-07 15:00:49 +0545

    Decided to use fastapi instead

--- backend/dataset_api/views.py
+++
@@ -1,7 +0,0 @@
-# Rest Framework
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.request import Request
-from rest_framework.response import Response
-
-from typing import Mapping
--- backend/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HomeView, HelloView
-
-urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
-    path("hello/", HelloView.as_view(), name="hello"),
-]
