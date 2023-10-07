b2dfc53501a9617752a176daeee016e9cb7b47f3
Nikita0728 <kunwornikita123@gmail.com>
2023-10-07 22:55:08 +0545

    heeeee

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -18,7 +18,7 @@ const Home = () => {
                 <div className='flex pl-[22rem] pt-[8rem]  justify-items-center'>
                   <div className="join flex items-center space-x-3">
                     <button className="btn join-item my-2 bg-slate-400 hover:bg-white text-black">
-                      
+                      Most Viewed Datasets
                       </button>
                     <button className="btn join-item my-2 bg-slate-400 hover:bg-white text-black">Recently Added 
                      Datasets</button>
