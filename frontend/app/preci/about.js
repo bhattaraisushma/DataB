a90c4f069a479523c838689bc56507f15771d24d
Nikita0728 <kunwornikita123@gmail.com>
2023-10-08 00:18:49 +0545

    precipitation

---
+++ frontend/app/preci/about.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1 className='text-gray-400 text-3xl'>Precipitation</h1>
+
+        <p className='text-stone-400'>
+        Precipitation refers to any form of water, liquid or solid, that falls from the atmosphere to the Earth's 
+           surface. It is a crucial component of the Earth's hydrological cycle and plays a significant role in 
+           shaping the climate, weather patterns, and the distribution of water resources on our planet.It has 
+           significant impacts on ecosystems, economies, and daily life.
+        </p>
+    </div>
+  )
+}
+
+export default About
