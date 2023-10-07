24280f316c6a0cc61ad427dd1a12d0edf1d1e0b8
kiyotone <kuwarkirtan36@gmail.com>
2023-10-08 03:40:14 +0545

    download button added

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -74,14 +74,14 @@ const Navbar = () => {
       <div className="navbar  bg-black sticky top-0">
         <div className="flex justify-between">
           <div>
-            <a className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl">Home</a>
-            <a className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl">About</a>
-            <a className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text-  
-             xl">Contact</a>
-            <a className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl">Feedback</a>
+            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl" onClick={()=>router.push('/')}>Home</div>
+            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl" onClick={()=>router.push('/temp')}>Temperature</div>
+            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text-  
+             xl" onClick={()=>router.push('/preci')}>Prepetation</div>
+            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl" onClick={()=>router.push('/search')}>Search</div>
           </div>
           <input type="text" className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem] w-[50rem] rounded-[12rem]" onChange={(e) => handleSearchChange(e)} onKeyDown={(e) => textKey(e)} />
           <p className='text-[2rem] absolute ml-[81rem]'> <AiOutlineSearch /></p>
