88c574a9590566e43716ba2cdd5ab7603b66075f
kiyotone <kunwarkirtan36@gmail.com>
2023-10-07 14:11:28 +0545

    normal mode

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -11,10 +11,10 @@ const Navbar = () => {
       <div className="navbar  bg-[#1d232a]">
   <div className="flex justify-between">
     <div>
-    <a className="btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Home</a>
-    <a className="btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Earth</a>
-    <a className="btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Sun</a>
-    <a className="btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Moon</a>
+    <a className="text-[#a6adba] btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Home</a>
+    <a className="text-[#a6adba] btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Earth</a>
+    <a className="text-[#a6adba] btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Sun</a>
+    <a className="text-[#a6adba] btn btn-ghost bg-[#1d232a] hover:bg-[#383f47] normal-case text-xl">Moon</a>
     </div>
     <input type ="text" className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem] w-[50rem] rounded-[12rem]" onChange={(e)=> change_seach(e.target.value)}/>
     <p className='text-[2rem] absolute ml-[81rem]'> <AiOutlineSearch/></p>
