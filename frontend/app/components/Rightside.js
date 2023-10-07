fd8b7a012894cc04d955088c268ff6947636c5a0
Nikita0728 <kunwornikita123@gmail.com>
2023-10-07 16:08:18 +0545

    committe

--- frontend/app/components/Rightside.js
+++ frontend/app/components/Rightside.js
@@ -6,18 +6,9 @@ function Rightside() {
   return (
     <div >
       <div className='pl-[40rem]'>
-      <Image className=" mt-7 w-[40rem] "src={photo}/>
+      <Image className=" mt-7 w-[60rem] pr-[7rem]"src={photo}/>
       </div>
-      <div>
-        <section>
-          <div className="join join-vertical">
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Most Viewed Datasets</button>
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Recently Added Datasets</button>
-            <button className="btn join-item my-2 bg-slate-400 hover:bg-white">Datasets by Organization</button>
-          </div>
       
-        </section>
-      </div>
     </div>
   )
 }
