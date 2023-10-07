43c6090b484c719c1e1689ff223f7bf43cbe2adc
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-10-08 02:44:58 +0545

    Overview

--- frontend/app/preci/about.js
+++ frontend/app/preci/about.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>
-        <h1 className='text-gray-400 text-3xl'>Precipitation</h1>
+    <div className="p-5">
+      <h1 className="text-gray-400 text-6xl mb-10">Precipitation</h1>
 
-        <p className='text-stone-400'>
-        Precipitation refers to any form of water, liquid or solid, that falls from the atmosphere to the Earth's 
-           surface. It is a crucial component of the Earth's hydrological cycle and plays a significant role in 
-           shaping the climate, weather patterns, and the distribution of water resources on our planet.It has 
-           significant impacts on ecosystems, economies, and daily life.
-        </p>
+      <p className="text-stone-400 text-lg text-justify">
+        Precipitation refers to any form of water, liquid or solid, that falls
+        from the atmosphere to the Earth's surface. It is a crucial component of
+        the Earth's hydrological cycle and plays a significant role in shaping
+        the climate, weather patterns, and the distribution of water resources
+        on our planet.It has significant impacts on ecosystems, economies, and
+        daily life.
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- frontend/app/preci/page.js
+++ frontend/app/preci/page.js
@@ -22,7 +22,7 @@ const Visual = () => {
   };
 
   return (
-    <div>
+    <div className="text-[#ddd]">
       <About />
       <div className="flex gap-10">
         <main>
@@ -52,6 +52,115 @@ const Visual = () => {
         </div>
 
         <p>Selected Year: {selectedYear}</p>
+      </div>
+      <div className="w-3/4 mx-auto py-4 mb-11 text-lg">
+        <h2 className="text-xl font-semibold mb-2">About Data:</h2>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">1.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">0.044 - 0.496 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents very light precipitation. It means that on
+            average, between 0.044 millimeters and 0.496 millimeters of
+            precipitation falls per day. This could be considered a minimal
+            amount of rain or other forms of precipitation.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">2.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">0.496 - 0.875 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents light precipitation. It means that on average,
+            between 0.496 millimeters and 0.875 millimeters of precipitation
+            falls per day. This is still a relatively small amount of rainfall.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">3.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">0.875 - 1.23 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents slightly heavier precipitation. It means that
+            on average, between 0.875 millimeters and 1.23 millimeters of
+            precipitation falls per day.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">4.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">1.23 - 1.547 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents moderate precipitation. It means that on
+            average, between 1.23 millimeters and 1.547 millimeters of
+            precipitation falls per day. This indicates a more significant
+            amount of rainfall.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">5.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">1.547 - 1.803 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range also represents moderate precipitation but at a slightly
+            higher intensity.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">6.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">1.803 - 2.023 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents moderately heavy precipitation. It means that
+            on average, between 1.803 millimeters and 2.023 millimeters of
+            precipitation falls per day.
+          </p>
+        </div>
+        <div className="mb-4">
+          <p className="mb-2">
+            <span className="font-semibold">7.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">2.023 - 2.341 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents heavy precipitation. It means that on average,
+            between 2.023 millimeters and 2.341 millimeters of precipitation
+            falls per day. This indicates a substantial amount of rainfall.
+          </p>
+        </div>
+        <div>
+          <p className="mb-2">
+            <span className="font-semibold">8.</span>
+          </p>
+          <p className="mb-1">
+            <span className="font-semibold">2.341 - 3.149 mm/day:</span>
+          </p>
+          <p className="ml-2">
+            This range represents very heavy precipitation. It means that on
+            average, between 2.341 millimeters and 3.149 millimeters of
+            precipitation falls per day. This is considered a significant amount
+            of rainfall and may be associated with heavy rain or storms.
+          </p>
+        </div>
       </div>
     </div>
   );
