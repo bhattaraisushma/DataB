afbd32276a0dc4034a2d45fac3a08a5b459f0c85
kiyotone <kuwarkirtan36@gmail.com>
2023-10-07 11:54:55 +0545

    update

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -16,7 +16,9 @@ export default function RootLayout({ children }) {
        
        
       <body className={inter.className}>
+      
       <Navbar/>
+        
        <Footer/>
        {children}</body>
     </html>
