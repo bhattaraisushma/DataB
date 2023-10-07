740e5a5f056c4396b7b06605596a49aea948a3e9
kiyotone <kunwarkirtan36@gmail.com>
2023-10-08 04:46:24 +0545

    new navbar

--- frontend/app/components/Data.js
+++ frontend/app/components/Data.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 
+
 function Data({data}) {
   
         const router = useRouter();
   return (
     <>
-    <div className='hover:scale-110 h-[9.9em] w-[50rem] duration-300 ease-in-out cursor-pointer' onClick={()=>router.push(data.url)}>
+    <div className='hover:scale-110 h-[9.9em] w-[50rem] duration-300 ease-in-out cursor-pointer'>
      <div className=' rounded-md bg-[#3b3b3b] h-full text-cyan-100 '>
         <div className='flex items-center space-x-6'>
 
-        <img className=' text-white w-[4rem] h-[3rem]' src={data.photo}/>
+        <img className=' text-white w-[4rem] h-[3rem]' src={data.photo} onError={`this.src"=../../public/default.png"`}/>
         <span className='text-lg'>{data.title}</span>
         
         </div>
