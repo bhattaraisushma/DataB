45a7b327aa6476139782188d3247020d2a82f102
kiyotone <kunwarkirtan36@gmail.com>
2023-10-07 16:58:04 +0545

    kirtan merge conflcit fix

--- frontend/app/search/page.js
+++ frontend/app/search/page.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      
+      <div>
+        <img  src="https://www.hhs.gov/sites/default/files/web/images/seal_blue_gold_hi_res.jpg">
+
+        </img>
+      </div>
+    </div>
   )
 }
 
