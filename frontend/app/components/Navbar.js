f87639631f09a2affadb4059697b8c3db1fbfb31
pr-a-sh-ant <prashantaryal07@gmail.com>
2023-10-08 08:56:19 +0545

    project finally builds

--- frontend/app/components/Navbar.js
+++ frontend/app/components/Navbar.js
@@ -1,14 +1,12 @@
-"use client"
-import { React } from 'react'
+"use client";
+import { React } from "react";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { setDisplay, setSearch } from './redux/features/mainSlice';
-import { usePathname, useRouter } from 'next/navigation';
-import api from './axios';
+import { useDispatch, useSelector } from "react-redux";
+import { setDisplay, setSearch } from "./redux/features/mainSlice";
+import { usePathname, useRouter } from "next/navigation";
+import api from "./axios";
 
 const Navbar = () => {
-
-
   const search = useSelector((state) => state.main.search);
 
   const dispatch = useDispatch();
@@ -17,17 +15,14 @@ const Navbar = () => {
 
   const pathname = usePathname();
 
-  const initial_date = useSelector((state)=> state.main.initial_date);
-  const final_date = useSelector((state)=> state.main.final_date);
+  const initial_date = useSelector((state) => state.main.initial_date);
+  const final_date = useSelector((state) => state.main.final_date);
 
-  
   const textKey = (e) => {
-
     if (e.keyCode == 13) {
       getData();
     }
-
-  }
+  };
 
   async function getData() {
     console.log("getting data...");
@@ -36,35 +31,30 @@ const Navbar = () => {
       query: search,
       limit: 10,
       initial_date: initial_date,
-      final_date:final_date,
-
-    }
+      final_date: final_date,
+    };
 
     let response = null;
 
     try {
-      response = await api.get('/search', {
-        params: params
-      })
-    } catch (error)
-    {
+      response = await api.get("/search", {
+        params: params,
+      });
+    } catch (error) {
       // handle error
     }
 
     const response_data = response.data;
     // console.log(response_data)
-    dispatch(setDisplay(response_data))
+    dispatch(setDisplay(response_data));
   }
 
-
   function handleSearchChange(e) {
-    console.log(pathname)
+    // console.log(pathname)
     if (!pathname.includes("/search")) {
-
-        router.push('/search')
-      
+      router.push("/search");
     }
-    
+
     dispatch(setSearch(e.target.value));
     // console.log(search);
   }
@@ -74,25 +64,45 @@ const Navbar = () => {
       <div className="navbar  bg-black sticky top-0">
         <div className="flex justify-between">
           <div>
-            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl" onClick={()=>router.push('/')}>Home</div>
-            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl" onClick={()=>router.push('/temp')}>Temperature</div>
-            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text-  
-             xl" onClick={()=>router.push('/preci')}>Precipitation</div>
-            <div className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
-             xl" onClick={()=>router.push('/search')}>Search</div>
+            <div
+              className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl"
+              onClick={() => router.push("/")}
+            >
+              Home
+            </div>
+            <div
+              className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl"
+              onClick={() => router.push("/temp")}
+            >
+              Temperature
+            </div>
+            <div
+              className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text-  
+             xl"
+              onClick={() => router.push("/preci")}
+            >
+              Precipitation
+            </div>
+            <div
+              className="text-[#656667] btn btn-ghost  hover:bg-[#edf5fd] normal-case text- 
+             xl"
+              onClick={() => router.push("/search")}
+            >
+              Search
+            </div>
           </div>
-          
-          <input type="text" placeholder='Search Data' className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem] w-[45rem] rounded-[12rem]" onChange={(e) => handleSearchChange(e)} onKeyDown={(e) => textKey(e)} />
 
-
-
-
+          <input
+            type="text"
+            placeholder="Search Data"
+            className=" text-[1rem] text-right pr-[5rem] text ml-[13rem] h-[2rem] w-[45rem] rounded-[12rem]"
+            onChange={(e) => handleSearchChange(e)}
+            onKeyDown={(e) => textKey(e)}
+          />
         </div>
-
       </div>
-
     </>
   );
 };
